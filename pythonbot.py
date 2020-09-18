27071b166857fcdef8a626fca98497046040af4c
KoKoOgit <ookevin14@gmail.com>
2020-09-18 09:53:45 +0630

    newstylecodingadded

--- pythonbot.py
+++ pythonbot.py
@@ -1,31 +1,31 @@
+
 from covid import Covid
 import smtplib 
+import os
 import time
-from threading import Timer
-import os 
+
 covid = Covid()
 email = os.environ['email']
-password = os.environ['password']
+password = os.password['password']
 server = smtplib.SMTP_SSL('smtp.gmail.com',465)
 server.login(email,password)
-server.sendmail(email,'mgkokooo2006@icloud.com',"Try")
+work = True
 
-def getdata():
-    global mm,c,d,r
-    mm = covid.get_status_by_country_id(28)
-    c = mm['confirmed']
-    d = mm['deaths']
-    r = mm['recovered']
-    time.sleep(5)
-    global mm2,c2,d2,r2
-    mm2 = covid.get_status_by_country_id(28)
-    c2 = mm['confirmed']
-    d2 = mm['deaths']
-    r2 = mm['recovered']
+while True:
+    while work == True:
+        mm = covid.get_status_by_country_id(28)
+        c = mm['confirmed']
+        d = mm['deaths']
+        r = mm['recovered']
+        time.sleep(2)
+        mm2 = covid.get_status_by_country_id(28)
+        c2 = mm2['confirmed']
+        d2 = mm2['deaths']
+        r2 = mm2['recovered']
+        if c != c2:
+            work = False
 
-def run():
-    getdata()
-    if c2 > c:
+    while work == False:
         server.sendmail(email,'mgkokooo2006@icloud.com',
         f"""confirmed-{c2}
         deaths-{d2}
@@ -34,6 +34,4 @@ def run():
         NewDeaths-{d2-d}
         NewRecoverd-{r2-r}
         """)
-        Timer(60,run).start()
-
-run()
+        work = True 
